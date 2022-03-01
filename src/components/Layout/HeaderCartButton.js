c4e61b4a308f9e407aa8813c85f802ae388dd762
SeCre827 <lefterisoikonomy@yahoo.gr>
2022-03-01 14:24:18 +0200

    Added Cart Reducer

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,8 +1,17 @@
 import React from "react";
+import { useContext } from "react";
+import CartContext from "../../store/cart-context";
+
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const cartCTX = useContext(CartContext);
+
+  const numberOfCartItems = cartCTX.items.reduce((cur, item) => {
+    return cur + item.amount;
+  }, 0);
+
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
